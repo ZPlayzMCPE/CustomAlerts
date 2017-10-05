bec3a9538fb9db1fbcef7b8014f7abeee328aabc
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-10-05 12:43:37 +0100

    Update MotdTask.php

--- src/CustomAlerts/MotdTask.php
+++ src/CustomAlerts/MotdTask.php
@@ -24,7 +24,7 @@ class MotdTask extends PluginTask {
         $this->counter = 0;
     }
     
-    public function onRun(int $ticks){
+    public function onRun(int $CurrentTick){
     	$cfg = $this->getOwner()->getConfig()->getAll();
     	$this->counter += 1;
     	if($this->counter >= $cfg["Motd"]["update-timeout"]){
